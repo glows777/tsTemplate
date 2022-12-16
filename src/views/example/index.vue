d2502633f65353ccf15c4b86f9f9701e42d08fd9
glows777 <1914426389@qq.com>
2022-12-16 23:10:09 +0800

    feat: 添加pinia，axios等的示例

--- src/views/example/index.vue
+++ src/views/example/index.vue
@@ -2,14 +2,15 @@
  * @Author: glows777 1914426389@qq.com
  * @Date: 2022-12-16 13:43:12
  * @LastEditors: glows777 1914426389@qq.com
- * @LastEditTime: 2022-12-16 15:30:54
+ * @LastEditTime: 2022-12-16 22:10:14
  * @FilePath: \vue-admin\src\views\example\index.vue
- * @Description: 
- * 
- * Copyright (c) 2022 by glows777 1914426389@qq.com, All Rights Reserved. 
+ * @Description: 本页仅用于展示如何使用
+ *
+ * Copyright (c) 2022 by glows777 1914426389@qq.com, All Rights Reserved.
 -->
 <script setup lang="ts">
 import { useCounterStore } from '~/store/counter'
+import { getListAPI } from '~/utils/api'
 
 const counterStore = useCounterStore()
 
@@ -17,15 +18,25 @@ const counterPlusOne = computed(() => counterStore.counterPlusOne)
 const actionCounterPlusOne = () => counterStore.increment()
 const randomizeCounter = () => counterStore.randomizeCounter()
 
-
-
+const getList = () => {
+  getListAPI().then((res) => {
+    console.log(res)
+  })
+}
 </script>
 
 <template>
-    <h1>Counter</h1>
-    <h2>{{counterStore.count}}</h2>
-    <h3>double: {{counterStore.doubleCounter}}</h3>
-    <h3>plus one : {{counterPlusOne}}</h3>
-    <n-button @click="actionCounterPlusOne">add one</n-button>
-    <n-button @click="randomizeCounter">randomize</n-button>
+  <h1>Counter</h1>
+  <h2>{{ counterStore.count }}</h2>
+  <h3>double: {{ counterStore.doubleCounter }}</h3>
+  <h3>plus one : {{ counterPlusOne }}</h3>
+  <el-button @click="actionCounterPlusOne">
+    add one
+  </el-button>
+  <el-button @click="randomizeCounter">
+    randomize
+  </el-button>
+  <el-button @click="getList">
+    网络请求
+  </el-button>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,15 +4,17 @@
  * @LastEditors: glows777 1914426389@qq.com
  * @LastEditTime: 2022-12-16 14:52:20
  * @FilePath: \vue-admin\src\views\home\index.vue
- * @Description: 
- * 
- * Copyright (c) 2022 by glows777 1914426389@qq.com, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2022 by glows777 1914426389@qq.com, All Rights Reserved.
 -->
 <script setup lang="ts">
 
 </script>
 
 <template>
-    <h1>主页</h1>
-    <RouterLink to="example">前往示例页面</RouterLink>
+  <h1>主页</h1>
+  <RouterLink to="example">
+    前往示例页面
+  </RouterLink>
 </template>

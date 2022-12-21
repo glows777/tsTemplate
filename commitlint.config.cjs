22f06d8b18e9f331dfd6e733187b284519c75b92
glows777 <1914426389@qq.com>
2022-12-21 15:55:27 +0800

    test: 测试提交规范

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
-      '@commitlint/config-conventional'
+    '@commitlint/config-conventional',
   ],
   rules: {
     'type-enum': [2, 'always', [
-      'build', 'ci', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'test'
+      'build', 'ci', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'test',
     ]],
     'type-case': [0],
     'type-empty': [0],
@@ -12,6 +12,6 @@ module.exports = {
     'scope-case': [0],
     'subject-full-stop': [0],
     'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72]
-  }
+    'header-max-length': [0, 'always', 72],
+  },
 }
